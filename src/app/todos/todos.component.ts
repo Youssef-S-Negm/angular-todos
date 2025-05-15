dc80db89a17e36f90974a2ee691e0a45e58cd5c3
Youssef Negm <youssef.negm2001@gmail.com>
2025-05-15 20:41:25 +0300

    feat: fetching todos on load

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { AddTodosComponent } from './add-todos/add-todos.component';
-import { PendingTodosComponent } from "./pending-todos/pending-todos.component";
-import { CompletedTodosComponent } from "./completed-todos/completed-todos.component";
+import { PendingTodosComponent } from './pending-todos/pending-todos.component';
+import { CompletedTodosComponent } from './completed-todos/completed-todos.component';
+import TodoService from '../firebase/todo.service';
 
 @Component({
   selector: 'app-todos',
   imports: [AddTodosComponent, PendingTodosComponent, CompletedTodosComponent],
   templateUrl: './todos.component.html',
 })
-export class TodosComponent {}
+export class TodosComponent implements OnInit {
+  private todoService = inject(TodoService);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    const subscription = this.todoService.getTodos$().subscribe();
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
+}
