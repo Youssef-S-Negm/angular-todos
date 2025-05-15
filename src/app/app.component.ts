e53cf84535bd1ad956a3d8b5654d774757687fc3
Youssef Negm <youssef.negm2001@gmail.com>
2025-05-15 23:22:41 +0300

    update: handled loading state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from "./header/header.component";
-import { TodosComponent } from "./todos/todos.component";
+import { Component, inject } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { TodosComponent } from './todos/todos.component';
+import TodoService from './firebase/todo.service';
+import { SpinnerComponent } from "./spinner/spinner.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, TodosComponent],
+  imports: [HeaderComponent, TodosComponent, SpinnerComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  private todoService = inject(TodoService);
+  isLoading = this.todoService.isLoading;
+}
