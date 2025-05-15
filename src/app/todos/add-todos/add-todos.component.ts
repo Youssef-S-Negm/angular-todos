b7ee44235e75fb04b1e9812327ac3200962b96a6
Youssef Negm <youssef.negm2001@gmail.com>
2025-05-15 19:03:39 +0300

    update: submitting todos

--- src/app/todos/add-todos/add-todos.component.ts
+++ src/app/todos/add-todos/add-todos.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import TodoService from '../../firebase/todo.service';
 
 @Component({
   selector: 'app-add-todos',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-todos.component.html',
 })
-export class AddTodosComponent {}
+export class AddTodosComponent {
+  private todoService = inject(TodoService);
+  private destroyRef = inject(DestroyRef);
+  title = signal('');
+
+  onSubmit() {
+    const subscription = this.todoService
+      .addTodo$({
+        dateCreated: new Date(),
+        priority: 1,
+        status: 'pending',
+        title: this.title(),
+      })
+      .subscribe();
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
+}
