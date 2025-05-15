e5ea61c5e639f6ef22e3cef609a2997bd02bd951
Youssef Negm <youssef.negm2001@gmail.com>
2025-05-15 19:03:11 +0300

    update: added todo service
    - implemented add todo only

---
+++ src/app/firebase/todo.service.ts
@@ -0,0 +1,23 @@
+import { inject, Injectable, signal } from '@angular/core';
+import Todo from '../models/todo.model';
+import {
+  addDoc,
+  collection,
+  collectionData,
+  Firestore,
+} from '@angular/fire/firestore';
+import { from, Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export default class TodoService {
+  private firestore = inject(Firestore);
+  private todosCollection = collection(this.firestore, 'todos');
+  private todos = signal<Todo[]>([]);
+  allTodos = this.todos.asReadonly();
+
+  addTodo$(todo: Todo) {
+    return from(addDoc(this.todosCollection, todo));
+  }
+}
