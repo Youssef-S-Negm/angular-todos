75a874f64fb572681cfa43b67195854951cee19f
Youssef Negm <youssef.negm2001@gmail.com>
2025-05-15 21:15:44 +0300

    update: addes Status type

--- src/app/models/todo.model.ts
+++ src/app/models/todo.model.ts
@@ -1,7 +1,9 @@
 export default interface Todo {
   title: string;
-  status: 'pending' | 'done';
+  status: Status;
   priority: 1 | 2;
   dateCreated: Date;
   id?: string;
 }
+
+export type Status = 'pending' | 'done';
