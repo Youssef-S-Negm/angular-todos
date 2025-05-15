7281bb8cc02e8a3122e888208719511bb0193e3a
Youssef Negm <youssef.negm2001@gmail.com>
2025-05-15 22:23:31 +0300

    update: sorting todos by date and priority

--- src/app/todos/pending-todos/pending-todos.component.ts
+++ src/app/todos/pending-todos/pending-todos.component.ts
@@ -1,17 +1,39 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { SearchPendingTodosComponent } from '../search-pending-todos/search-pending-todos.component';
 import TaskService from '../../firebase/todo.service';
 import { TodoComponent } from '../todo/todo.component';
+import { FormsModule } from '@angular/forms';
+
+type SortOptionValue =
+  | 'date-asc'
+  | 'date-desc'
+  | 'priority-asc'
+  | 'priority-desc';
 
 @Component({
   selector: 'app-pending-todos',
-  imports: [SearchPendingTodosComponent, TodoComponent],
+  imports: [SearchPendingTodosComponent, TodoComponent, FormsModule],
   templateUrl: './pending-todos.component.html',
   styleUrl: './pending-todos.component.css',
 })
 export class PendingTodosComponent {
   private taskService = inject(TaskService);
+  selectedSortOption = signal<SortOptionValue>('date-asc');
   todos = computed(() =>
-    this.taskService.allTodos().filter((todo) => todo.status === 'pending')
+    this.taskService
+      .allTodos()
+      .filter((todo) => todo.status === 'pending')
+      .sort((a, b) => {
+        switch (this.selectedSortOption()) {
+          case 'date-asc':
+            return a.dateCreated > b.dateCreated ? 1 : -1;
+          case 'date-desc':
+            return a.dateCreated > b.dateCreated ? -1 : 1;
+          case 'priority-asc':
+            return a.priority > b.priority ? 1 : -1;
+          case 'priority-desc':
+            return a.priority > b.priority ? -1 : 1;
+        }
+      })
   );
 }
