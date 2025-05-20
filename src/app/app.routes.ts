2f0fd59bad9ba8b3aa5e1fcdda45f2dee66afe7e
Youssef Negm <youssef.negm2001@gmail.com>
2025-05-20 18:37:17 +0300

    update: added app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { routes as AuthRoutes } from './auth/auth.routes';
+import { authGuard } from './auth/auth.guards';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'todos', pathMatch: 'full' },
+  {
+    path: 'todos',
+    loadComponent: () =>
+      import('./todos/todos.component').then((module) => module.TodosComponent),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'auth',
+    component: AuthComponent,
+    children: AuthRoutes,
+  },
+];
