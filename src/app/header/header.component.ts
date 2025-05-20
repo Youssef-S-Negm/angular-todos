f4440549e4b182cc3b1e8e23f7060565a2caa28a
Youssef Negm <youssef.negm2001@gmail.com>
2025-05-20 18:39:18 +0300

    update: showing logout button according to the user state

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  private authService = inject(AuthService);
+  private destroyRef = inject(DestroyRef);
+  private router = inject(Router);
+
+  isAuthenticated = signal(false);
+
+  ngOnInit(): void {
+    const subscription = this.authService.user.subscribe({
+      next: (user) => {
+        this.isAuthenticated.set(!!user);
+
+        if (!user) {
+          this.router.navigate(['auth', 'login'], { replaceUrl: true });
+        }
+      },
+    });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
+
+  onClickLogout() {
+    this.authService.logout();
+  }
+}
