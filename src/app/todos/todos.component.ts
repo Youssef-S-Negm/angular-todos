6ad39ca5af1641fc7598de1481e0c1f55330df02
Youssef Negm <youssef.negm2001@gmail.com>
2025-05-20 18:30:40 +0300

    update: handled loading state in todos component

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -3,15 +3,22 @@ import { AddTodosComponent } from './add-todos/add-todos.component';
 import { PendingTodosComponent } from './pending-todos/pending-todos.component';
 import { CompletedTodosComponent } from './completed-todos/completed-todos.component';
 import TodoService from '../firebase/todo.service';
+import { SpinnerComponent } from '../spinner/spinner.component';
 
 @Component({
   selector: 'app-todos',
-  imports: [AddTodosComponent, PendingTodosComponent, CompletedTodosComponent],
+  imports: [
+    AddTodosComponent,
+    PendingTodosComponent,
+    CompletedTodosComponent,
+    SpinnerComponent,
+  ],
   templateUrl: './todos.component.html',
 })
 export class TodosComponent implements OnInit {
   private todoService = inject(TodoService);
   private destroyRef = inject(DestroyRef);
+  isLoading = this.todoService.isLoading;
 
   ngOnInit(): void {
     const subscription = this.todoService.getTodos$().subscribe();
