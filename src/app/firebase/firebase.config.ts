853ae0db9306b8665af83300fd354d20ac764ec4
Youssef Negm <youssef.negm2001@gmail.com>
2025-05-21 22:19:48 +0300

    update: added firestore url with query enabled

--- src/app/firebase/firebase.config.ts
+++ src/app/firebase/firebase.config.ts
@@ -8,6 +8,9 @@ const FIREBASE_CONFIG = Object.freeze({
   measurementId: 'G-PN3K2JKLVY',
 });
 
-export const TODOS_URL = `https://firestore.googleapis.com/v1/projects/${FIREBASE_CONFIG.projectId}/databases/(default)/documents/todos`;
+const FIRESTORE_URL = `https://firestore.googleapis.com/v1/projects/${FIREBASE_CONFIG.projectId}/databases/(default)/documents`;
+
+export const TODOS_URL = `${FIRESTORE_URL}/todos`;
+export const FIRESTORE_URL_QUERY_ENABLED = `${FIRESTORE_URL}:runQuery`;
 export const SIGNUP_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FIREBASE_CONFIG.apiKey}`;
 export const LOGIN_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_CONFIG.apiKey}`;
